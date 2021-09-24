eb8ac997348a5bc25be19441950fc3138496a92a
p0lich <a.polikarpov2014@gmail.com>
2021-09-24 22:48:43 +0400

    init

---
+++ Epam.DigitalLibrary/Epam.DigitalLibrary.ConsolePL/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Epam.DigitalLibrary.ConsolePL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(
+                    "1: Add note;\n" +
+                    "2: Delete note;\n" +
+                    "3: Browse catalog;\n" +
+                    "4: Search by name;\n" +
+                    "5: Sort by year(in forward order);\n" +
+                    "6: Sort by year(in reverse order);\n" +
+                    "7: Search all books by author;\n" +
+                    "8: Search all patents by inventor;\n" +
+                    "9: Searhc all books and patents by author;\n" +
+                    "10: Show books by given character set(group by publisher);\n" +
+                    "11: Group by publication year;\n" +
+                    "0: Exit."
+                    );
+            }
+        }
+    }
+}
