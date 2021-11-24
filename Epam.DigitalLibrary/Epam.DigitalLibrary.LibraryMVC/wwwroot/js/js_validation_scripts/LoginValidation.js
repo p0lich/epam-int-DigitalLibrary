2f9df9a02ba1caec009b3e32ea956ce690a9a676
p0lich <a.polikarpov2014@gmail.com>
2021-11-25 01:38:55 +0400

    add client validation for login

---
+++ Epam.DigitalLibrary/Epam.DigitalLibrary.LibraryMVC/wwwroot/js/js_validation_scripts/LoginValidation.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+    jQuery.validator.addMethod("isLoginCorrect", function (value, element) {
+        let regCheck = /^[A-Za-z]+((_?[A-Za-z0-9]+)|([A-Za-z0-9]*))+[A-Za-z0-9]$/;
+        return this.optional(element) || regCheck.test(value);
+    }, "login pattern mismatch");
+
+    $("form[name='loginForm']").validate({
+        rules: {
+            Login: {
+                required: true,
+                minlength: 3,
+                maxlength: 20,
+                isLoginCorrect: true,
+            },
+
+            Password: {
+                required: true,
+                minlength: 3,
+                maxlength: 20,
+            },
+        },
+
+        messages: {
+            Login: {
+                required: "Login is required",
+            },
+            Password: {
+                required: "Password is required",
+            },
+        },
+    });
+});
