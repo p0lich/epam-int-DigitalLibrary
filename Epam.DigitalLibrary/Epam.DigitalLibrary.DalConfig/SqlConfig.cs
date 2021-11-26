40e70d99e38f8bf959648eb80f245e3044a5a3a3
p0lich <a.polikarpov2014@gmail.com>
2021-11-26 15:25:58 +0400

    add config project

--- Epam.DigitalLibrary/Epam.DigitalLibrary.DalConfig/SqlConfig.cs
+++ Epam.DigitalLibrary/Epam.DigitalLibrary.DalConfig/SqlConfig.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Epam.DigitalLibrary.DalConfig
 {
-    class SqlConfig
+    public static class SqlConfig
     {
+        
     }
 }
