5f19fa107f05d6f3015484d3ca0c46ad88972fb3
p0lich <a.polikarpov2014@gmail.com>
2021-12-16 21:54:59 +0400

    add controller for patent

--- Epam.DigitalLibrary/Epam.DigitalLibrary.LibraryWebApi/Controllers/ApiPatentController.cs
+++ Epam.DigitalLibrary/Epam.DigitalLibrary.LibraryWebApi/Controllers/ApiPatentController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Epam.DigitalLibrary.Entities;
+using Epam.DigitalLibrary.Entities.Models.PatentModels;
+using Epam.DigitalLibrary.LogicContracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,36 +17,142 @@ namespace Epam.DigitalLibrary.LibraryWebApi.Controllers
     [ApiController]
     public class ApiPatentController : ControllerBase
     {
-        // GET: api/<ApiPatentController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly ILogger<ApiPatentController> _logger;
+        private readonly INoteLogic _logic;
+
+        public ApiPatentController(ILogger<ApiPatentController> logger, INoteLogic logic)
         {
-            return new string[] { "value1", "value2" };
+            _logger = logger;
+            _logic = logic;
         }
 
-        // GET api/<ApiPatentController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(Guid id)
         {
-            return "value";
+            if (!IsPatentExist(id, out Patent patent))
+            {
+                return NotFound($"Cannot find note with id: {id}");
+            }
+
+            return Ok(_logic.GetPatentById(id));
         }
 
-        // POST api/<ApiPatentController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] PatentInputViewModel patentModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("model is not valid");
+            }
+
+            Patent patent = new Patent(
+                name: patentModel.Name,
+                authors: patentModel.AuthorsId.Select(authId => _logic.GetAuthor(authId)).ToList(),
+                country: patentModel.Country,
+                registrationNumber: patentModel.RegistrationNumber,
+                applicationDate: patentModel.ApplicationDate,
+                publicationDate: patentModel.PublicationDate,
+                pagesCount: patentModel.PagesCount,
+                objectNotes: patentModel.ObjectNotes
+                );
+
+            int addResult = _logic.AddNote(patent, out Guid noteId);
+
+            return Ok(FillCreateError(addResult));
         }
 
-        // PUT api/<ApiPatentController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(Guid id, [FromBody] PatentInputViewModel patentModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("model is not valid");
+            }
+
+            if (!IsPatentExist(id, out Patent patent))
+            {
+                return NotFound($"Cannot find note with id: {id}");
+            }
+
+            Patent updatePatent = new Patent(
+                name: patentModel.Name,
+                authors: patentModel.AuthorsId.Select(authId => _logic.GetAuthor(authId)).ToList(),
+                country: patentModel.Country,
+                registrationNumber: patentModel.RegistrationNumber,
+                applicationDate: patentModel.ApplicationDate,
+                publicationDate: patentModel.PublicationDate,
+                pagesCount: patentModel.PagesCount,
+                objectNotes: patentModel.ObjectNotes
+                );
+
+            int updateResult = _logic.UpdateNote(id, updatePatent);
+
+            return Ok(FillUpdateError(updateResult));
         }
 
-        // DELETE api/<ApiPatentController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(Guid id)
         {
+            if (!IsPatentExist(id, out Patent patent))
+            {
+                return NotFound($"Cannot find note with id: {id}");
+            }
+
+            bool deleteResult = _logic.MarkForDelete(patent);
+
+            return Ok(FillDeleteError(deleteResult));
+        }
+
+        private bool IsPatentExist(Guid noteId, out Patent foundPatent)
+        {
+            foundPatent = _logic.GetPatentById(noteId);
+            return foundPatent is not null;
+        }
+
+        private string FillCreateError(int addResult)
+        {
+            string errorMessage = null;
+
+            if (addResult == ResultCodes.NoteExist)
+            {
+                errorMessage = "Cannot add. Same patent already exist";
+            }
+
+            if (addResult == ResultCodes.Error)
+            {
+                errorMessage = "Cannot add. Unexpected error";
+            }
+
+            return errorMessage;
+        }
+
+        private object FillUpdateError(int updateResult)
+        {
+            string errorMessage = null;
+
+            if (updateResult == ResultCodes.NoteExist)
+            {
+                errorMessage = "Cannot update. Same patent already exist";
+            }
+
+            if (updateResult == ResultCodes.Error)
+            {
+                errorMessage = "Cannot update. Unexpected error";
+            }
+
+            return errorMessage;
+        }
+
+        private object FillDeleteError(bool deleteResult)
+        {
+            string errorMessage = null;
+
+            if (deleteResult == ResultCodes.ErrorDelete)
+            {
+                errorMessage = "Cannot delete. Undexpected error";
+            }
+
+            return errorMessage;
         }
     }
 }
