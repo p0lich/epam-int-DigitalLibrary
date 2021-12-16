d29361f7499595dddfeac173edfda2ecd48cfbaf
p0lich <a.polikarpov2014@gmail.com>
2021-12-17 03:18:13 +0400

    Update ApiNewspaperController.cs

--- Epam.DigitalLibrary/Epam.DigitalLibrary.LibraryWebApi/Controllers/ApiNewspaperController.cs
+++ Epam.DigitalLibrary/Epam.DigitalLibrary.LibraryWebApi/Controllers/ApiNewspaperController.cs
@@ -25,24 +25,138 @@ namespace Epam.DigitalLibrary.LibraryWebApi.Controllers
             _logic = logic;
         }
 
-        [HttpGet("{id}")]
-        public IActionResult Get(Guid id)
+        [HttpGet("GetNewspaperReleases/{id}")]
+        public IActionResult GetNewspaperReleases(Guid id)
         {
+            if (!IsNewspaperExist(id, out Newspaper newspaper))
+            {
+                return NotFound("Release with such id isnt exist");
+            }
+
+            var releases = _logic.GetNewspaperReleases(id).Select(r => new NewspaperReleaseDetailsViewModel()
+            {
+                ID = r.ID,
+                PagesCount = r.PagesCount,
+                Number = r.Number,
+                ReleaseDate = r.ReleaseDate,
+                IsDeleted = r.IsDeleted,
+            });
+
+            return Ok(releases);
         }
 
-        [HttpPost]
-        public IActionResult Post([FromBody] NewspaperInputViewModel newspaperModel)
+        [HttpGet("GetRelease/{id}")]
+        public IActionResult GetRelease(Guid id)
         {
+            if (!IsNewspaperExist(id, out Newspaper newspaper))
+            {
+                return NotFound("Such release isnt exist");
+            }
+
+            return Ok(_logic.GetNewspaperById(id));
         }
 
-        [HttpPut("{id}")]
-        public IActionResult Put(Guid id, [FromBody] NewspaperInputViewModel value)
+        [HttpPost("PostNewspaper")]
+        public IActionResult PostNewspaper([FromBody] NewspaperInputViewModel newspaperModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            int addResult = _logic.AddNewspaperRelease(newspaperModel, out Guid id);
+
+            return Ok(FillCreateMessage(addResult, id));
         }
 
-        [HttpDelete("{id}")]
-        public IActionResult Delete(Guid id)
+        [HttpPost("PostNewspaperRelease")]
+        public IActionResult PostNewspaperRelease([FromBody] NewspaperFullInput newspaperModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            Newspaper newspaper = new Newspaper(
+                name: newspaperModel.NewspaperInput.Name,
+                publicationPlace: newspaperModel.NewspaperInput.PublicationPlace,
+                publisher: newspaperModel.NewspaperInput.Publisher,
+                publicationDate: newspaperModel.NewspaperInput.PublicationDate,
+                pagesCount: newspaperModel.ReleaseInput.PagesCount,
+                objectNotes: newspaperModel.NewspaperInput.ObjectNotes,
+                number: newspaperModel.ReleaseInput.Number,
+                releaseDate: newspaperModel.ReleaseInput.ReleaseDate,
+                iSSN: newspaperModel.NewspaperInput.ISSN
+                );
+
+            int addresult = _logic.AddNote(newspaper, out Guid id);
+
+            return Ok(FillCreateMessage(addresult, id));
+        }
+
+        [HttpPut("UpdateNewspaper/{id}")]
+        public IActionResult UpdateNewspaper(Guid id, [FromBody] NewspaperInputViewModel newspaperModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            int updateResult = _logic.UpdateNewspaperInfo(id, newspaperModel);
+
+            return Ok(FillUpdateError(updateResult));
+        }
+
+        [HttpPut("UpdateNewspaperRelease/{id}")]
+        public IActionResult UpdateNewspaperRelease(Guid id, [FromBody] NewspaperReleaseInputViewModel newspaperReleaseModel)
+        {
+            if (!IsNewspaperExist(id, out Newspaper newspaper))
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            Newspaper newspaperUpdate = new Newspaper(
+                name: newspaper.Name,
+                releaseId: newspaper.ReleaseId,
+                publicationPlace: newspaper.PublicationPlace,
+                publisher: newspaper.Publisher,
+                publicationDate: newspaper.PublicationDate,
+                pagesCount: newspaperReleaseModel.PagesCount,
+                objectNotes: newspaper.ObjectNotes,
+                number: newspaperReleaseModel.Number,
+                releaseDate: newspaperReleaseModel.ReleaseDate,
+                iSSN: newspaper.ISSN
+                );
+
+            int updateResult = _logic.UpdateNote(id, newspaperUpdate);
+
+            return Ok(FillUpdateError(updateResult));
+        }
+
+        [HttpDelete("DeleteNewspaper/{id}")]
+        public IActionResult DeleteNewspaper(Guid id)
+        {
+            bool deleteResult = _logic.MarkForDeleteNewspaperRelease(id);
+
+            return Ok(FillDeleteError(deleteResult));
+        }
+
+        [HttpDelete("DeleteNewspaperRelease/{id}")]
+        public IActionResult DeleteNewspaperRelease(Guid id)
+        {
+            if (!IsNewspaperExist(id, out Newspaper newspaper))
+            {
+                return BadRequest();
+            }
+
+            bool deleteResult = _logic.MarkForDelete(newspaper);
+
+            return Ok(deleteResult);
         }
 
         private bool IsNewspaperExist(Guid noteId, out Newspaper foundNewspaper)
@@ -51,21 +165,21 @@ namespace Epam.DigitalLibrary.LibraryWebApi.Controllers
             return foundNewspaper is not null;
         }
 
-        private object FillCreateError(int addResult)
+        private object FillCreateMessage(int addResult, Guid id)
         {
-            string errorMessage = null;
+            string message = id.ToString();
 
             if (addResult == ResultCodes.NoteExist)
             {
-                errorMessage = "Cannot add. Same newspaper already exist";
+                message = "Cannot add. Same newspaper already exist";
             }
 
             if (addResult == ResultCodes.Error)
             {
-                errorMessage = "Cannot add. Unexpected error";
+                message = "Cannot add. Unexpected error";
             }
 
-            return errorMessage;
+            return message;
         }
 
         private object FillUpdateError(int updateResult)
