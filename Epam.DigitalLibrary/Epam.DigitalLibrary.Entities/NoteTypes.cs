f90cdd30e2a8c1da1ed7ddef0b3b33cb755479e0
p0lich <a.polikarpov2014@gmail.com>
2021-12-17 19:20:33 +0400

    edit methods for search

---
+++ Epam.DigitalLibrary/Epam.DigitalLibrary.Entities/NoteTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Epam.DigitalLibrary.Entities
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum NoteTypes : short
+    {
+        [Description("None")]
+        None = 0,
+
+        [Description("Book")]
+        Book = 1,
+
+        [Description("Newspaper")]
+        Newspaper = 2,
+
+        [Description("Patent")]
+        Patent = 3,
+    }
+}
--- Epam.DigitalLibrary/Epam.DigitalLibrary.LibraryWebApi/Controllers/SearchController.cs
+++ Epam.DigitalLibrary/Epam.DigitalLibrary.LibraryWebApi/Controllers/SearchController.cs
@@ -1,5 +1,6 @@
 ﻿using Epam.DigitalLibrary.Entities;
 using Epam.DigitalLibrary.Entities.Models;
+using Epam.DigitalLibrary.Entities.Models.SearchModels;
 using Epam.DigitalLibrary.LibraryWebApi.Models;
 using Epam.DigitalLibrary.LogicContracts;
 using Microsoft.AspNetCore.Mvc;
@@ -28,70 +29,16 @@ namespace Epam.DigitalLibrary.LibraryWebApi.Controllers
             _logic = logic;
         }
 
-        [HttpGet("GetNotes")]
-        public PagingList<NoteLink> GetNotes(string type, string name, int pageNumber = 1, int elementsCount = 20, int minPages = 0, int maxPages = short.MaxValue)
+        [HttpPost("GetNotes")]
+        public List<ShortNote> GetNotes([FromBody] SearchRequest searchRequest)
         {
-            var notes = _logic.GetCatalog();
-
-            if (!string.IsNullOrEmpty(type))
-            {
-                notes = notes.Where(n => n.GetType() == GetNoteType(type)).ToList();
-            }
-
-            var shortNotes = notes.Select(n => new NoteLink
-            {
-                Type = n.GetType().ToString(),
-                Id = n.ID,
-                Name = n.Name,
-                PagesCount = n.PagesCount
-            }).Where(n => n.PagesCount >= minPages && n.PagesCount <= maxPages);          
-
-            if (!string.IsNullOrEmpty(name))
-            {
-                shortNotes = shortNotes.Where(n => n.Name.Contains(name));
-            }
-
-            if (pageNumber > shortNotes.Count() / elementsCount + 1)
-            {
-                throw new ValidationException("page index was out of bounds");
-            }
-
-            var notesOnPage = PagingList<NoteLink>.GetPageItems(shortNotes.ToList(), pageNumber, elementsCount);
-
-            return notesOnPage;
+            return _logic.GetFilteredShortNotes(searchRequest, searchRequest.Type);
         }
 
         [HttpGet("GetAuthors")]
-        public IEnumerable<Author> GetAuthors(string name)
+        public IEnumerable<Author> GetAuthors(string namePattern)
         {
-            IEnumerable<Author> authors = _logic.GetAvailableAuthors();
-
-            if (!string.IsNullOrEmpty(name))
-            {
-                authors = authors.Where(a => string.Format($"{a.FirstName} {a.LastName}").Contains(name));
-            }
-
-            return authors;
-        }
-
-        private Type GetNoteType(string type)
-        {
-            if (type == "Book")
-            {
-                return typeof(Book);
-            }
-
-            if (type == "Newspaper")
-            {
-                return typeof(Newspaper);
-            }
-
-            if (type == "Patent")
-            {
-                return typeof(Patent);
-            }
-
-            throw new ArgumentException();
+            return _logic.GetFilteredAuthors(namePattern);
         }
     }
 }
