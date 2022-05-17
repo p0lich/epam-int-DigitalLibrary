7d76c06a110b9864e48a853eb53134518fb99c20
p0lich <a.polikarpov2014@gmail.com>
2022-05-17 09:48:02 +0400

    remove commentaries, unused config

--- Epam.DigitalLibrary/Epam.DigitalLibrary.LibraryMVC/Startup.cs
+++ Epam.DigitalLibrary/Epam.DigitalLibrary.LibraryMVC/Startup.cs
@@ -29,7 +29,6 @@ namespace Epam.DigitalLibrary.LibraryMVC
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<INoteLogic>(new LibraryLogic(Configuration.GetConnectionString("SSPIConnString")));
